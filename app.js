f69d448ebcf6b4f8008a5cb0daedb06d4971cd67
MaximoV <maximoverdi21@gmail.com>
2025-05-24 22:46:12 -0300

    Header and hero responsive

---
+++ app.js
@@ -0,0 +1,19 @@
+const burguerBtn = document.getElementById('burguer-btn');
+const navMenu = document.getElementById('nav');
+
+// Alternar clase "show" al hacer clic en el botón hamburguesa
+burguerBtn.addEventListener('click', () => {
+    navMenu.classList.toggle('show');
+});
+
+// Controlar el nav al redimensionar la ventana
+function handleResize() {
+    if (window.innerWidth >= 900) {
+        navMenu.classList.add('show');
+    } else {
+        navMenu.classList.remove('show');
+    }
+}
+
+window.addEventListener('resize', handleResize);
+handleResize(); // Llamada inicial
